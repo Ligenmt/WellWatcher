9df2da9d357deb225ef863336e83b08bf36de7f4
ligenmt <176478745@qq.com>
2016-09-05 20:22:47 +0800

    update

---
+++ Wellwatcher/app/src/main/java/com/ligen/wellwatcher/listener/DragItemChangeListener.java
@@ -0,0 +1,9 @@
+package com.ligen.wellwatcher.listener;
+
+/**
+ * Created by ligen on 2016/8/21.
+ */
+public interface DragItemChangeListener {
+
+    void onDragItemChange(int dragSrcPosition, int dragPosition);
+}
